bb8795ebe656a7ddf7e6c1e1ab2ca5965d98f105
Shaitanbabai <rebootleo0522@gmail.com>
2024-08-10 17:42:22 +0300

    DJ03 homework done

--- One/news/views.py
+++ One/news/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def home(request):
-    news = Post.objects.all()
+    news = Post.objects.all().order_by('-pub_date')
     return render(request, 'news/news.html', {'news': news})
